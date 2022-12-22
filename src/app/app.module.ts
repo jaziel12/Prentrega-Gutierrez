09aa5c25500527516973f1883e5451fd0e868764
Jaziel Gutierrez <jazielgutierrez50@gmail.com>
2022-12-22 12:34:36 -0300

    Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './Shared/Layout/header/header.component';
+import { PageWrapperComponent } from './Shared/Layout/page-wrapper/page-wrapper.component';
+import { MyMaterialModule } from './Shared/Modules/my-material.module';
+import { StudentsPageComponent } from './Pages/students-page/students-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StudentDialogComponent } from './Shared/Components/student-dialog/student-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PageWrapperComponent,
+    StudentsPageComponent,
+    StudentDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
